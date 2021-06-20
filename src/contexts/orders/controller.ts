7eb74e8f995e362f1402cbbc83d6124c0e005b20
Cesar Pena <cesar18pena@gmail.com>
2021-06-20 16:40:20 -0400

    feat(order): get all orders and single one + testing some ideas

---
+++ src/contexts/orders/controller.ts
@@ -0,0 +1,10 @@
+export class OrderController {
+  
+  constructor() {
+    
+  }
+
+
+
+
+}
--- src/contexts/orders/routes.ts
+++ src/contexts/orders/routes.ts
@@ -1,34 +1,41 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
+import { Prisma } from "@prisma/client";
 import { prisma } from '../../server';
 
-export const routes = (app: FastifyInstance, options: FastifyPluginOptions): void => {
+type OrderCreate = Prisma.OrderCreateInput;
+
+export const routes = (app: FastifyInstance, options: FastifyPluginOptions, done: any): void => {
 
   app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
-    const allUsers = await prisma.user.findMany({
-      take: 1,
-      select: {
-        id: true,
-        name: true,
-        profile: {
-          select: {
-            id: true,
-            bio: true,
-          }
+    const allOrders = await prisma.order.findMany();
+    reply.send(allOrders);
+  });
+
+  app.get('/:id', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { id } = request.params as any;
+    const orderId = parseInt(id, 10);
+    const selectedOrder = await prisma.order.findUnique({
+      where: {
+        id: orderId,
+      },
+    });
+
+    !selectedOrder ? reply.send({
+        data: null,
+        errors: {
+          message: `Not user with provided ID: ${orderId}`
         }
-      }
-    })
+    }) : null;
 
-    reply.send({ users: allUsers });
-  });
-
-  app.get('/:id', (request: FastifyRequest, reply: FastifyReply) => {
+    reply.send({data: selectedOrder, errors: null});
 
   });
 
-  app.post('/', (request: FastifyRequest, reply: FastifyReply) => {
-
+  app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    const payload: OrderCreate = request.body as any;
+    const createdOrder = await prisma.order.create({ data: payload });
+    reply.send(createdOrder);
   });
-
 
   app.put('/:id', (request: FastifyRequest, reply: FastifyReply) => {
 
@@ -38,4 +45,5 @@ export const routes = (app: FastifyInstance, options: FastifyPluginOptions): voi
 
   });
 
+  done();
 }
